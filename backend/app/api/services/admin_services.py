d2202cc4fcc2baf01ee70395d3ab7050906639be
ChavdarKostadinov98 <ckaivelcro@gmail.com>
2024-05-14 19:30:20 +0300

    Update Field parameter in admin services

--- backend/app/api/services/admin_services.py
+++ backend/app/api/services/admin_services.py
@@ -2,7 +2,7 @@ from pydantic import Field
 from backend.app.data.database import read_query
 
 
-def get_user_by_id(user_id: int = Field):
+def get_user_by_id(user_id: int = Field(gt=0)):
     sql = "SELECT * FROM users WHERE user_id = %s"
     result = read_query(sql, (user_id,))
     return result
