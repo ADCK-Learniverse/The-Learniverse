4450f968353135dc95d389d125f27492481124e7
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-05-14 13:00:51 +0300

    Update database

---
+++ backend/app/data/creditentials.py
@@ -0,0 +1,5 @@
+host="ac-forum-database.cxsom6qwak60.eu-north-1.rds.amazonaws.com"
+user="AlexandurAdmin"
+password="MasterPython"
+port=3306
+database="learnplatform"
---
+++ backend/app/data/database.py
@@ -0,0 +1,30 @@
+import mysql.connector
+from creditentials import *
+
+mydb = mysql.connector.connect(
+    host=f"{host}",
+    user=f"{user}",
+    password=f"{password}",
+    port=f"{port}",
+    database=f"{database}",
+)
+
+
+def read_query(sql: str, sql_params=()):
+    cursor = mydb.cursor()
+    cursor.execute(sql, sql_params)
+    return cursor.fetchall()
+
+
+def insert_query(sql: str, sql_params=()):
+    cursor = mydb.cursor()
+    cursor.execute(sql, sql_params)
+    mydb.commit()
+    return "success"
+
+
+def update_query(sql: str, sql_params=()):
+    cursor = mydb.cursor()
+    cursor.execute(sql, sql_params)
+    mydb.commit()
+    return "success"
