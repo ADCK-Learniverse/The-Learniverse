fd6d737b3e80cde2e61b363db17dd769fde6cabf
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-05-15 13:38:50 +0300

    Update creditentials

--- backend/app/data/creditentials.py
+++
@@ -1,5 +0,0 @@
-host="ac-forum-database.cxsom6qwak60.eu-north-1.rds.amazonaws.com"
-user="AlexandurAdmin"
-password="MasterPython"
-port=3306
-database="learnplatform"
--- backend/app/data/database.py
+++ backend/app/data/database.py
@@ -1,12 +1,19 @@
 import mysql.connector
-from backend.app.data.creditentials import host, user, password, port, database
+from dotenv import dotenv_values
+
+env_vars = dotenv_values(".env")
+HOST = env_vars.get("HOST")
+USER = env_vars.get("USER")
+PASSWORD = env_vars.get("PASSWORD")
+PORT = env_vars.get("PORT")
+DATABASE = env_vars.get("DATABASE")
 
 mydb = mysql.connector.connect(
-    host=f"{host}",
-    user=f"{user}",
-    password=f"{password}",
-    port=f"{port}",
-    database=f"{database}",
+    host=f"{HOST}",
+    user=f"{USER}",
+    password=f"{PASSWORD}",
+    port=f"{PORT}",
+    database=f"{DATABASE}",
 )
 
 
