1628e5c788153002a157e5dbd906284b0d5ae82b
AlexandurDaskalov <AlexandurDaskalov11@gmail.com>
2024-06-07 21:29:40 +0300

    Update utils

--- backend/app/api/utils/newsletter_utilities.py
+++ backend/app/api/utils/newsletter_utilities.py
@@ -1,14 +1,16 @@
 from backend.app.data.database import read_query
 from backend.app.api.services.register_services import send_emails
 
-subscribers = read_query('SELECT email FROM newsletter')
+
 def new_course_newsletter(topic):
+    subscribers = read_query('SELECT email FROM newsletter')
     send_emails(subscribers[0],
                 'Courses', f"A new course regarding {topic} has been released!",
                 "<h3>A new course is waiting for you.</h3>")
 
 
 def removed_course_newsletter():
+    subscribers = read_query('SELECT email FROM newsletter')
     send_emails(subscribers[0],
                 'Courses', f"A Course has been removed from our platform!",
                 "<h3>No longer a part of the Learniverse.</h3>")
